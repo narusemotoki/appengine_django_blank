e6c2e1b7a5779f91c519bdc91f5e36bedc662117
Naruse Motoki <motoki@naru.se>
2013-06-23 20:12:35 +0900

    Django1.5でエラーになりアクセスできないのを修正

--- appengine_django_blank/settings.py
+++ appengine_django_blank/settings.py
@@ -1,17 +1,19 @@
+DEBUG = False
+
+ALLOWED_HOSTS = ['localhost', 'your-app-id.appspot.com']
+
+INSTALLED_APPS = ('appengine_django_blank')
+
+ROOT_URLCONF = 'appengine_django_blank.urls'
+
+SECRET_KEY = 'Change this string'
+
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
 )
 
-INSTALLED_APPS = (
-    'appengine_django_blank'
-)
-
-ROOT_URLCONF = 'appengine_django_blank.urls'
+CACHE_BACKEND = 'memcached:///'
 
 import os
 ROOT_PATH = os.path.dirname(__file__)
-TEMPLATE_DIRS = (
-    ROOT_PATH + '/templates',
-)
-
-CACHE_BACKEND = 'memcached:///'
+TEMPLATE_DIRS = (ROOT_PATH + '/templates',)
